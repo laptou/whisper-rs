19199a8d9bd98cb9b0f11aa22b81fb391d0f2896
Ibiyemi Abiodun <ibiyemi@intulon.com>
2022-12-05 11:45:25 -0500

    testing time

--- examples/ree.rs
+++ examples/ree.rs
@@ -1,27 +1,57 @@
-use tch::{nn::VarStore, Device, Tensor, Kind};
-use whisper::{self, model::Whisper};
+use tch::{nn::VarStore, Device, Kind, Tensor};
+use tokenizers::Tokenizer;
+use whisper::{
+    self,
+    decode::{DecodeOptions, DecodeTask},
+    model::{ModelDims, Whisper},
+};
 
 pub fn main() {
-    let tensors = tch::Tensor::read_npz("oof.npz").unwrap();
-    Tensor::save_multi(&tensors[..], "oof.ot").unwrap();
+    // let tensors = tch::Tensor::read_npz("oof.npz").unwrap();
+    // Tensor::save_multi(&tensors[..], "oof.ot").unwrap();
+
     let mut vars = VarStore::new(Device::cuda_if_available());
-    // ModelDimensions(n_mels=80, n_audio_ctx=1500, n_audio_state=512,
-    // n_audio_head=8, n_audio_layer=6, n_vocab=51864, n_text_ctx=448,
-    // n_text_state=512, n_text_head=8, n_text_layer=6)
     let model = Whisper::new(
         vars.root(),
-        51864,
-        1500,
-        512,
-        8,
-        6,
-        448,
-        512,
-        8,
-        6,
-        Tensor::empty(&[1, 1], (Kind::Float, Device::cuda_if_available())),
+        ModelDims {
+            n_vocab: 51864,
+            n_audio_ctxs: 1500,
+            n_audio_states: 512,
+            n_audio_heads: 8,
+            n_audio_layers: 6,
+            n_text_ctxs: 448,
+            n_text_states: 512,
+            n_text_heads: 8,
+            n_text_layers: 6,
+        },
     );
+
     vars.load("oof.ot").unwrap();
-    
-    println!("{:?}", vars.variables());
+    // println!("vocab size = {}", tok.get_vocab_size(true));
+
+    let mut dt = DecodeTask::new(
+        &model,
+        DecodeOptions {
+            task: whisper::tokenize::Task::Transcribe,
+            sample_len: None,
+            token_extract_mode: whisper::decode::TokenExtractMode::BeamSearch {
+                beam_size: 5,
+                patience: 1.0,
+            },
+            len_penalty: None,
+        },
+    )
+    .unwrap();
+
+    let audio = whisper::audio::load_audio("test/data/jfk_resampled.wav").unwrap();
+    let audio = whisper::audio::pad_or_trim(&audio);
+
+    let mel_filter = whisper::audio::mel_filter();
+
+    let mel_audio = whisper::audio::log_mel_spectrogram(&audio, &mel_filter);
+    let mel_audio = mel_audio.unsqueeze(0);
+
+    println!("audio shape = {:?}", mel_audio.size());
+
+    dt.run(mel_audio).unwrap();
 }
